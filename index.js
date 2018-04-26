6379619c2b0e6831a78ce277ab4c0572efd927b9
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-04-26 20:14:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code in this file!
 const currentUser = 'Grace Hopper';
 
-const welcomeMessage = 'Welcome to Flatbook, ';
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
