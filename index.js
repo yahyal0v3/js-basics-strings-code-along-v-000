d6d3359d3829556bc7953686ca6afbb468b41ae4
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-04-26 20:33:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = 'Welcome, ';
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}`;
